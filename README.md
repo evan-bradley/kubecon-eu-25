f3c4a45c3bf0b6cd1fdf05531fac7b8d37b49286
Evan Bradley <11745660+evan-bradley@users.noreply.github.com>
2025-03-13 23:45:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Customize Your Own OpenTelemetry Collector: An Introduction to OCB
+
+This repository contains diagrams and examples related to the OCB
+presentation that will be given at KubeCon EU 2025 in London, UK.
---
+++ render.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+directories=(diagram)
+
+docker build -t mermaid-cli-fonts .
+
+for dir in ${directories}; do
+    pushd ${dir}
+    
+    mkdir -p output
+
+    files=$(find . -iname '*.mmd' -type f)
+    rm -f output/*.png
+    for file in $files; do
+        docker run --rm -u $(id -u):$(id -g) -v $(pwd):/data mermaid-cli-fonts -c config.json -s 20 -i ${file} -o output/$(basename ${file}).png
+    done
+
+    popd
+done
